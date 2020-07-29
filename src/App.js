d0c66a3d325a2c1dd1cb423a27a5424dfee2c063
Paola Hernández <60892852+paolaA2494@users.noreply.github.com>
2020-07-29 14:33:55 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./App.css";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>Mi primero proyecto en React, que genial!!</p>
+      <input type="text"/>
+      <ul>
+        <li>primer objeto</li>
+
+  
+      </ul>
+    </div>
+  );
+}
